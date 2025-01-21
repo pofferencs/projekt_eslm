1a39a5272f7eea5751aaf2bb3f777a731cb285f4
Korcsok Roland József <korcsokrolandjozsef-30143@taszi.hu>
2025-01-21 13:27:54 +0100

    Backend - Match Update - kezdetleges

--- Project/e_Sport_Projekt/backend/controllers/updateControllers.js
+++ Project/e_Sport_Projekt/backend/controllers/updateControllers.js
@@ -206,9 +206,38 @@ const picture_linkUpdate = async (req, res) => {
     }
 };
 
+const matchUpdate = async (req,res) =>{
+    const {id, tem1_id, tem2_id, tnt_id, status, place, dte, details, winner, rslt} = req.body;
 
-// const matchUpdate
-// const picture_linkUpdate
+    // státuszok: unstarted, started, ended, suspended
+    try{
+        
+         if(status != "ended" || status !=="started"){
+            // unstarted, suspended
+                // dte, place, details
+
+            
+
+
+        }
+        else if(status == "ended"){
+            // ended
+                // winner, rslt, details
+            
+        }
+        else{
+            // started
+                // details
+        }
+        
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({ message: "Hiba a fetch során!" });
+    }
+    
+}
+
 // const teamUpdate
 // const team_membershipUpdate
 // const tournamentUpdate
@@ -219,8 +248,8 @@ module.exports = {
     eventUpdate,
     gameUpdate,
     pictureUpdate,
-    picture_linkUpdate
-    // matchUpdate
+    picture_linkUpdate,
+    matchUpdate
     // teamUpdate
     //team_membershipUpdate
     //tournamentUpdate
--- Project/e_Sport_Projekt/backend/routes/updateRoutes.js
+++ Project/e_Sport_Projekt/backend/routes/updateRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {userUpdate, applicationUpdate, eventUpdate, gameUpdate, pictureUpdate, picture_linkUpdate} = require('../controllers/updateControllers')
+const {userUpdate, applicationUpdate, eventUpdate, gameUpdate, pictureUpdate, picture_linkUpdate, matchUpdate} = require('../controllers/updateControllers')
 
 router.patch('/user',userUpdate);
 router.patch('/application', applicationUpdate);
 router.patch('/event',eventUpdate);
 router.patch('/game',gameUpdate);
-// router.patch('/match',);
+router.patch('/match',matchUpdate);
 router.patch('/picture', pictureUpdate);
 router.patch('/picturelink', picture_linkUpdate);
 // router.patch('/team',);
