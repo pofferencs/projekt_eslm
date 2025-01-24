765d6a1c2a7733a3dc35c244a39c5901e9856da6
pofferencs <gedovics16@gmail.com>
2025-01-24 13:14:31 +0100

    Törlések

---
+++ Project/e_Sport_Projekt/backend/routes/deleteRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+//const {applicationDelete} = require('../controllers/applicationController');
+//const { eventDelete } = require('../controllers/eventController');
+//const { gameDelete } = require('../controllers/gameController');
+//const { pictureDelete } = require('../controllers/pictureController');
+//const { tournamentDelete } = require('../controllers/tournamentController');
+
+//router.delete('/application', applicationDelete)
+//router.delete('/event', eventDelete);
+//router.delete('/game', gameDelete);
+//router.delete('/picture', pictureDelete);
+//router.delete('/tournament', tournamentDelete);
+
+
+module.exports = router
