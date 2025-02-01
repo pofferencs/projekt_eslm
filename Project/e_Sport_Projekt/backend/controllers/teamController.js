c4266c6f2e2b29f5093b12140e82b97b9651e270
Korcsok Roland József <korcsokrolandjozsef-30143@taszi.hu>
2025-02-01 20:53:56 +0100

    .

--- Project/e_Sport_Projekt/backend/controllers/teamController.js
+++ Project/e_Sport_Projekt/backend/controllers/teamController.js
@@ -41,7 +41,6 @@ const teamUpdate = async (req,res)=>{
             console.log(error);
             return res.status(500).json({message: "Hiba a fetch során!"})
         }
-<<<<<<< Updated upstream
     }
 }
 
@@ -63,18 +62,9 @@ const teamDelete = async (req, res) =>{
     }
 }
 
-const teamCreate =  async (req,res) =>{
-    const {short_name, full_name, creator_id} = req.body;
-    
-=======
-    }
-}
-
 const teamInsert = async (req,res) =>{
     const { short_name, full_name, creator_id} = req.body;
 
-
->>>>>>> Stashed changes
 }
 
 module.exports={
