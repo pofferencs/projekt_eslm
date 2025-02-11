d73c52b7183f58b4df2ba72a7d330e9dcde1aadb
pofferencs <gedovics16@gmail.com>
2025-02-11 19:51:46 +0100

    Regiszráció: Jelszó vizsgálata (Business Rules szerint)

--- Project/e_Sport_Projekt/backend/controllers/userController.js
+++ Project/e_Sport_Projekt/backend/controllers/userController.js
@@ -178,9 +178,9 @@ const userReg = async (req, res)=>{
 
 
 
-const paswrd = "asd";
-const specChars = ["*", "@", "_"];
-const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// const paswrd = "asd";
+// const specChars = ["*", "@", "_"];
+// const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
         
 // const hashedPass = bcrypt.hashSync(paswrd, 10);
