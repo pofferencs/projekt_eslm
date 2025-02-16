07d310db5c8063903fb0728e46ae22956ffb4532
Korcsok Roland József <korcsokrolandjozsef-30143@taszi.hu>
2025-02-16 22:26:00 +0100

    Update matchController.js

--- Project/e_Sport_Projekt/backend/controllers/matchController.js
+++ Project/e_Sport_Projekt/backend/controllers/matchController.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const { hianyzoAdat, validalasFuggveny, hianyzoAdatFuggveny } = require('../functions/conditions');
+const { validalasFuggveny, hianyzoAdatFuggveny } = require('../functions/conditions');
 const prisma = new PrismaClient();
 
 const matchList = async (req, res) => {
