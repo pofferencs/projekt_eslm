309762dab4dcb09a6d8aa90d2cc76384334e37e4
Korcsok Roland József <korcsokrolandjozsef-30143@taszi.hu>
2025-03-01 21:14:38 +0100

    userReg - fullname

--- Project/e_Sport_Projekt/frontend/src/components/user/UserLogin.jsx
+++ Project/e_Sport_Projekt/frontend/src/components/user/UserLogin.jsx
@@ -42,6 +42,16 @@ function UserLogin() {
   const [formData, setFormData] = useState(formObj);
   const writeData = (e) => {
     setFormData((prevState) => ({ ...prevState, [e.target.id]: e.target.value }));
+
+    const [identifier, setIdentifier] = useState("");
+    const [isEmail, setIsEmail] = useState(false);
+
+    const valtozasKezelo = (e) =>{
+      const value = e.target.value;
+      setIdentifier(value);
+      setIsEmail(value.includes("@"));
+    }
+
   }
 
   return (
@@ -64,6 +74,7 @@ function UserLogin() {
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm/6 font-medium text-indigo-600">E-mail cím vagy felhasználónév</label>
+              
               <div className="mt-2">
                 <input type="email" name="email" id="email" autoComplete="email" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
               </div>
