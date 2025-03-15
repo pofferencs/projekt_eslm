6c9f473a23886f1558a2d2b6c1a43270e9c0e84b
pofferencs <gedovics16@gmail.com>
2025-03-15 17:06:26 +0100

    Keresés fix fix

--- Project/e_Sport_Projekt/frontend/src/components/common/SearchU.jsx
+++ Project/e_Sport_Projekt/frontend/src/components/common/SearchU.jsx
@@ -41,7 +41,7 @@ function SearchU() {
                     <label htmlFor="username" className="block text-sm/6 font-medium text-indigo-600">Felhasználónév</label>
 
                     <div className="mt-2">
-                        <input type="text" name="username" id="username" autoComplete="username" required value={getData.usr_name} onChange={writeData} className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input type="text" name="username" id="username" autoComplete="username" required value={searchInput} onChange={writeData} className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                     <div>
                         <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Keresés</button>
