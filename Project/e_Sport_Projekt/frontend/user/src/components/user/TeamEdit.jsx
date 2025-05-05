81dbc50d251ad7cca56ef9b0cb308518325c43fd
Korcsok Roland József <korcsokrolandjozsef-30143@taszi.hu>
2025-05-05 11:56:02 +0200

    teamedit kezdés

--- Project/e_Sport_Projekt/frontend/user/src/components/user/TeamEdit.jsx
+++ Project/e_Sport_Projekt/frontend/user/src/components/user/TeamEdit.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import { useContext, useEffect } from "react"
+import { Navigate, useLocation, useParams } from "react-router-dom"
+import UserContext from "../../context/UserContext"
+
 
 function TeamEdit() {
+    const {isAuthenticated, isLoading, refresh} = useContext(UserContext)
+    const teamId = useParams();
+
+    useEffect(()=>{
+        if(!isAuthenticated){
+            Navigate('/')
+        }
+        
+        
+
+
+    },[teamId, isAuthenticated])
+    
   return (
     <div>TeamEdit</div>
   )
